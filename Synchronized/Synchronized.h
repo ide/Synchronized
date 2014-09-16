7e0a1b2e84cf473bc4c3a5b72298b7dfb6c7ed96
James Ide <ide@jameside.com>
2014-09-16 11:58:36 -0700

    Initial Commit

---
+++ Synchronized/Synchronized.h
@@ -0,0 +1,19 @@
+//
+//  Synchronized.h
+//  Synchronized
+//
+//  Created by James Ide on 9/16/14.
+//  Copyright (c) 2014 ide. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Synchronized.
+FOUNDATION_EXPORT double SynchronizedVersionNumber;
+
+//! Project version string for Synchronized.
+FOUNDATION_EXPORT const unsigned char SynchronizedVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Synchronized/PublicHeader.h>
+
+
